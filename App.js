f33c6c4f78f78eda62a6fc6bb4ea71c6277a9132
manisrini <srinimani678@gmail.com>
2022-02-25 10:02:04 +0530

    add dynamic phonenum

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
-import React, { useState } from 'react';
-import { Alert } from 'react-native';
+import React, {useState} from 'react';
+import {Alert} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import PhoneNumber from './screens/PhoneNumber';
 import VerifyCode from './screens/VerifyCode';
 import Authenticated from './screens/Authenticated';
-import { useDispatch } from 'react-redux';
+import {useDispatch} from 'react-redux';
 
 export default function App() {
-
   const dispatch = useDispatch();
   const [confirm, setConfirm] = useState(null);
   const [authenticated, setAuthenticated] = useState(false);
 
   async function signIn(phoneNumber) {
     try {
-      console.log(phoneNumber)
-      const confirmation = await auth().signInWithPhoneNumber('+91 8939430888');
-      
+      console.log(phoneNumber);
+      const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+
       setConfirm(confirmation);
-      
     } catch (error) {
-      
-      alert("error");
+      alert('error');
     }
   }
 
@@ -30,20 +27,20 @@ export default function App() {
       await confirm.confirm(code);
       setConfirm(null);
       dispatch({
-        type : "over"
-      })
+        type: 'over',
+      });
     } catch (error) {
       alert('Invalid code');
     }
   }
 
-  auth().onAuthStateChanged((user) => {
-    if(user) {
+  auth().onAuthStateChanged(user => {
+    if (user) {
       setAuthenticated(true);
     } else {
       setAuthenticated(false);
     }
-  })
+  });
 
   if (authenticated) return <Authenticated />;
 
